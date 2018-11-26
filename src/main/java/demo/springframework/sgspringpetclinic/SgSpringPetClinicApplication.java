cae4e65d85cc68716ece0bf7bb7981abfd65682d
sahil <sahilgarg5794@gmail.com>
2018-11-27 01:09:46 +0530

    initial checkin

---
+++ src/main/java/demo/springframework/sgspringpetclinic/SgSpringPetClinicApplication.java
@@ -0,0 +1,12 @@
+package demo.springframework.sgspringpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgSpringPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgSpringPetClinicApplication.class, args);
+	}
+}
